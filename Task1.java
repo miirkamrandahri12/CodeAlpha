18f57a17ce9791d5b0a0ac33edf4d6f827479805
miirkamrandahri12 <87007508+miirkamrandahri12@users.noreply.github.com>
2024-04-29 16:42:58 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,58 @@
+package codeApha.com;
+import java.util.Scanner;
+public class Task1 {
+	
+	
+	
+	
+	
+
+		
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter the number of students: ");
+
+	        int students_Count = sc.nextInt();
+	        double[] student_Grades = new double[students_Count];
+
+	        for (int i = 0; i < students_Count; i++) {
+	            System.out.print("Enter grade for student " + (i + 1) + " between 0 and 100 : ");
+	            double grade = sc.nextDouble();
+	            if(grade>=0 && grade<=100) {
+	                student_Grades[i] = grade;
+	            }else{
+	                System.out.println("Invalid Input.");
+	                i--;
+	            }
+	        }
+
+	        studentResults(student_Grades);
+
+	        sc.close();
+	    }
+	    private static void studentResults(double[] grades_Array) {
+	        double total_score = 0;
+	        double max_score = -1;
+	        double min_score = 101;
+
+	        for (double studentScore : grades_Array) {
+	            total_score += studentScore;
+
+	            if (studentScore > max_score) {
+	                max_score = studentScore;
+	            }
+
+	            if (studentScore < min_score) {
+	                min_score = studentScore;
+	            }
+	        }
+
+	        double average =  total_score / grades_Array.length;
+
+	        System.out.println("Highest Score of Students is : " + (int)max_score);
+	        System.out.println("Lowest Score of Students is: " + (int)min_score);
+	        System.out.println("Average Student Score is: " + average);
+	    }
+	}
+
+
