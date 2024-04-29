0d5a43171b4fd723e4d15ee1c56668156fa66991
miirkamrandahri12 <87007508+miirkamrandahri12@users.noreply.github.com>
2024-04-29 16:46:49 +0500

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,14 +1,7 @@
 package codeApha.com;
 import java.util.Scanner;
 public class Task1 {
-	
-	
-	
-	
-	
-
-		
-	    public static void main(String[] args) {
+	 public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
 	        System.out.print("Enter the number of students: ");
 
